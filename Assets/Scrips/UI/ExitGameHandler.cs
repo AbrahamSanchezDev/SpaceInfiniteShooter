e923d4a55a5b9cd568b00e5776fcf952cb8d0cd7
Abraham Sanchez <abraham_gto@hotmail.com>
2026-03-08 21:52:14 -0600

    Updated visuals for enemies and UI

---
+++ Assets/Scrips/UI/ExitGameHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ExitGameHandler : MonoBehaviour {
+    private Button _exitButton;
+
+    private void Awake() {
+        // Automatically get the Button component attached to this GameObject
+        _exitButton = GetComponent<Button>();
+
+        // Add the listener via code instead of the Inspector
+        _exitButton.onClick.AddListener(QuitApplication);
+    }
+
+    private void QuitApplication() {
+        Debug.Log("Exiting Application...");
+
+        // This works for standalone builds (.exe, .app)
+        Application.Quit();
+
+        // This allows the exit button to work while testing inside the Unity Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    private void OnDestroy() {
+        // Clean up the listener when the object is destroyed to prevent memory leaks
+        if (_exitButton != null) {
+            _exitButton.onClick.RemoveListener(QuitApplication);
+        }
+    }
+}
---
+++ Assets/Scrips/UI/TimedDisable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDisable : MonoBehaviour {
+    [Tooltip("Time in seconds before the object is disabled.")]
+    public float disableTime = 3.0f;
+
+    private float _timer;
+
+    private IEnumerator Start() {
+        yield return new WaitForSeconds(disableTime);
+        gameObject.SetActive(false);
+    }
+}
